c163a93bfa47767e36751c511546fb2aff5061a1
sahilkumar27 <sahildhn27@gmail.com>
2026-03-01 03:01:16 +0530

    Pushed the code for Product of array except self

--- DSA/ProductOfArrayExceptSelf.cpp
+++ DSA/ProductOfArrayExceptSelf.cpp
@@ -6,12 +6,14 @@ vector<int> productExpectedSelf(vector<int>& nums)
     int n = nums.size();
     vector<int> ans(n, 1);  //Create a vector of size n where every element is initialized to 1.
 
+    // I Compute prefix (left) product and store it in ans vector.
     int leftProduct = 1;
     for(int i=0;i<n;i++){
         ans[i]=leftProduct;
         leftProduct *= nums[i];
     }
 
+    // I traverse from the right and maintain a suffix (right) product, multiplying it with the corresponding prefix product in the ans vector to get the final product for each index.
     int rightProduct =1;
     for (int i = n-1; i >= 0; i--)
     {
@@ -20,6 +22,8 @@ vector<int> productExpectedSelf(vector<int>& nums)
     }
     return ans;
     
+    // Time Complexity: O(n) - We traverse the input array twice, once for calculating the prefix product and once for calculating the suffix product.
+    // Space Complexity: O(1) - We are using the ans vector to store the final result, and we are not using any additional data structures that grow with the input size. The space used by the ans vector is not considered extra space since it is required for the output.
 }
 
 int main()
